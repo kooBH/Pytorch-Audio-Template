3fa8409d7fd45833a4631a8697e7f06f9288965a
kbh <nine4409@sogang.ac.kr>
2021-09-26 16:14:15 +0900

    modify dataset

---
+++ src/Dataset.py
@@ -0,0 +1,28 @@
+import os, glob
+import torch
+
+class TestsetModel(torch.utils.data.Dataset):
+    def __init__(self, hp, is_train=True):
+        self.hp = hp
+        self.root = hp.data.root
+
+        if is_train :
+            self.list_data= [x for x in glob.glob(os.path.join(self.root,'train','*.pt'))]
+        else :
+            self.list_data= [x for x in glob.glob(os.path.join(self.root,'test','*.pt'))]
+
+
+
+    def __getitem__(self, index):
+        data_item = self.list_data[index]
+
+        # Process data_item if necessary.
+
+        data = data_item
+
+        return data
+
+    def __len__(self):
+        return len(self.data_list)
+
+
